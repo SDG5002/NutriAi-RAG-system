6b3161ce54d9c342f1d0355e1a1326b4660b5cfe
Sudesh Gosavi <sudeshgosavi5002@gmail.com>
2026-01-16 16:33:35 +0530

    Resolving the issue of the deployment on render

--- backend/app.py
+++ backend/app.py
@@ -152,7 +152,7 @@ def ask_question(request: QueryRequest):
 
 
 
-if __name__ == "__main__":
-    import uvicorn
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("app:app", host="0.0.0.0", port=port)
+# if __name__ == "__main__":
+#     import uvicorn
+#     port = int(os.environ.get("PORT", 8000))
+#     uvicorn.run("app:app", host="0.0.0.0", port=port)
